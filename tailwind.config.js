cf4625e78385d1c7f620dd9fa9456d7fc4a4ee77
Brightly Dunsford <131114729+techocea@users.noreply.github.com>
2023-09-29 12:27:30 +0530

    Add files via upload
    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "15px",
+      },
+
+      colors: {
+        accent: "#FF8F9C",
+        blackish: "#1b1b1b",
+      },
+    },
+  },
+  plugins: [],
+};
